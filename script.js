fae76e4c0119134b2caadc3fd12df6c2c9d928fe
charlotte <charlottedyt@gmail.com>
2023-03-30 21:30:49 -0400

    updated the event listener and changed header styles

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 document.querySelectorAll('.indexIntro').forEach((div) => {
-    div.addEventListener('click', (e) => {
-        e.target.querySelector("p")?.classList.toggle('hidden');
-      })
+  div.addEventListener('click', (e) => {
+    if (div.contains(e.target)) {
+      div.querySelector("p")?.classList.toggle('hidden');
+    }
+  })
 })
